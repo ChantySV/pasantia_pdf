0a7b806cbadc07cd6bca922875941a5e2697be34
Chanty <Santiago_SV@outlook.es>
2023-12-18 20:35:00 -0400

    Modularizacion2

--- BD.sql
+++ BD.sql
@@ -3,27 +3,23 @@ CREATE DATABASE Repositorio;
 USE Repositorio;
 
 CREATE TABLE Documentos (
-    id INT AUTO_INCREMENT,
+    id_documento INT AUTO_INCREMENT PRIMARY KEY,
     tipo VARCHAR(255),
-    carrera VARCHAR(255),
+    facultad varchar(255),
+    carrera VARCHAR(255),    
     titulo VARCHAR(255),
     autor VARCHAR(255),
-    year YEAR,
-    ruta_pdf VARCHAR(255),
-    PRIMARY KEY (id)
+    año YEAR,
+    sede varchar(255),
+    ruta_pdf VARCHAR(255)
 );
 
-INSERT INTO Documentos (tipo, carrera, titulo, autor, year, ruta_pdf)
-VALUES ('Proyecto de grado', 'Ingeniería en redes y telecomunicaciones', 'DISEÑO DE RED CON ERBIUM DOPED 
-FIBER AMPLIFIER Y TECNOLOGÍA 
-GIGABIT PASSIVE OPTICAL NETWORK 
-CON SERVICIOS DE BANDA ANCHA EN 
-LA ZONA SAN ANTONIO ALTO
-CASO: COOPERATIVA COTEL R.L.', 'Valeria Iris Tapia Moreno
-', 2022, 'C:\Users\vergh\OneDrive\Escritorio\Repositorio_upds\pdf\proyecto1.pdf');
-
-select * from Documentos;
-DELETE FROM Documentos WHERE id = 4;
+INSERT INTO Documentos (tipo, facultad, carrera, titulo, autor, año, sede, ruta_pdf)
+VALUES ('Proyecto de grado', 'Ingenieria', 'Ingeniería en redes y telecomunicaciones', 
+'DISEÑO DE RED CON ERBIUM DOPED FIBER AMPLIFIER 
+Y TECNOLOGÍA GIGABIT PASSIVE OPTICAL NETWORK CON SERVICIOS DE BANDA ANCHA EN LA ZONA SAN ANTONIO ALTO
+CASO: COOPERATIVA COTEL R.L.', 
+'Valeria Iris Tapia Moreno', 2022, 'La Paz', 'proyecto1.pdf');
 
 create table usuarios (
 id_usuario int auto_increment primary key,
@@ -32,9 +28,3 @@ contraseña varchar (60)
 );
 insert into usuarios  (nombre, contraseña)
 values('sorel', '1234');
-
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'sorel';
-FLUSH PRIVILEGES;
-
-
-SELECT VERSION();
